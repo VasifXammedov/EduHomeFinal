467eb1af2aae8127d17206c9ac226f2576afa884
VasifXammedov <vasifvkh@code.edu.az>
2021-01-02 03:14:27 +0400

    Login Logoutu yaratdiq

--- EDUHOME/Controllers/AccountController.cs
+++ EDUHOME/Controllers/AccountController.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EDUHOME.Models;
 using EDUHOME.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EDUHOME.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
@@ -22,6 +32,23 @@ namespace EDUHOME.Controllers
         public async Task<IActionResult> Register(RegisterVM register)
         {
             if (!ModelState.IsValid) return View();
+            AppUser newUser = new AppUser
+            {
+                Fullname = register.Fullname,
+                Email=register.Email,
+                UserName=register.Username
+
+            };
+            IdentityResult identityResult= await _userManager.CreateAsync(newUser, register.Password);
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            await _signInManager.SignInAsync(newUser, true);
             return RedirectToAction("Index", "Home");
         }
     }
