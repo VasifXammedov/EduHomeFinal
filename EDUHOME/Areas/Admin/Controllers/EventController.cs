78313c456782c04b473c6ec6482f25065a22aca8
VasifXammedov <vasifvkh@code.edu.az>
2021-01-03 05:40:45 +0400

    Eventin Create,Deleted ve updetini yaratdim

--- EDUHOME/Areas/Admin/Controllers/EventController.cs
+++ EDUHOME/Areas/Admin/Controllers/EventController.cs
@@ -13,18 +13,18 @@ using Microsoft.EntityFrameworkCore;
 namespace EDUHOME.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class TeacherController : Controller
+    public class EventController : Controller
     {
         private readonly AppDbContext _db;
         private readonly IWebHostEnvironment _env;
-        public TeacherController(AppDbContext db, IWebHostEnvironment env)
+        public EventController(AppDbContext db, IWebHostEnvironment env)
         {
             _db = db;
             _env = env;
         }
         public IActionResult Index()
         {
-            return View(_db.Teachers.Where(c => c.IsDeleted == false).OrderByDescending(c => c.Id).ToList());
+            return View(_db.LatestPostDetails.Where(c => c.IsDeleted == false).OrderByDescending(c => c.Id).ToList());
         }
 
         #region Create
@@ -36,37 +36,37 @@ namespace EDUHOME.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Teacher teacher)
+        public async Task<IActionResult> Create(LatestPostDetail events)
         {
             //if (!ModelState.IsValid) return View();
-            bool isExist = _db.Teachers.Where(c => c.IsDeleted == false)
-                   .Any(c => c.Name.ToLower() == teacher.Name.ToLower());
+            bool isExist = _db.LatestPostDetails.Where(c => c.IsDeleted == false)
+                   .Any(c => c.Title.ToLower() == events.Title.ToLower());
             if (isExist)
             {
-                ModelState.AddModelError("Name", "Bu Mellim artiq movcuddur");
+                ModelState.AddModelError("Name", "Bu event artiq movcuddur");
                 return View();
             }
-            if (teacher.Photo == null)
+            if (events.Photo == null)
             {
                 ModelState.AddModelError("", "Ayeee wekili elave ele!!!");
                 return View();
             }
-            if (!teacher.Photo.IsImage())
+            if (!events.Photo.IsImage())
             {
-                ModelState.AddModelError("", "Bunu yaratmaq ucun wekil tipi yarat!!!");
+                ModelState.AddModelError("", "Event yaratmaq ucun wekil tipi yarat!!!");
                 return View();
             }
-            if (!teacher.Photo.MaxSize(200))
+            if (!events.Photo.MaxSize(200))
             {
                 ModelState.AddModelError("", "Wekilin olcusu 200kb-dan az olmalidi!!!");
                 return View();
             }
 
-            string folder = Path.Combine("assets", "img", "teacher");
-            string fileName = await teacher.Photo.SaveImgAsync(_env.WebRootPath, folder);
-            teacher.Image = fileName;
-            teacher.IsDeleted = false;
-            await _db.Teachers.AddAsync(teacher);
+            string folder = Path.Combine("assets", "img", "event");
+            string fileName = await events.Photo.SaveImgAsync(_env.WebRootPath, folder);
+            events.Image = fileName;
+            events.IsDeleted = false;
+            await _db.LatestPostDetails.AddAsync(events);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -79,10 +79,10 @@ namespace EDUHOME.Areas.Admin.Controllers
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null) return NotFound();
-            Teacher teacher = _db.Teachers
+            LatestPostDetail events = _db.LatestPostDetails
                 .FirstOrDefault(c => c.IsDeleted == false && c.Id == id);
-            if (teacher == null) return NotFound();
-            return View(teacher);
+            if (events == null) return NotFound();
+            return View(events);
         }
 
         [HttpPost]
@@ -91,10 +91,10 @@ namespace EDUHOME.Areas.Admin.Controllers
         public async Task<IActionResult> DeletePost(int? id)
         {
             if (id == null) return NotFound();
-            Teacher teacher = _db.Teachers
+            LatestPostDetail events = _db.LatestPostDetails
                 .FirstOrDefault(c => c.IsDeleted == false && c.Id == id);
-            if (teacher == null) return NotFound();
-            teacher.IsDeleted = true;
+            if (events == null) return NotFound();
+            events.IsDeleted = true;
 
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -107,45 +107,45 @@ namespace EDUHOME.Areas.Admin.Controllers
         public IActionResult Update(int? id)
         {
             if (id == null) return NotFound();
-            Teacher teacher = _db.Teachers
+            LatestPostDetail events = _db.LatestPostDetails
                 .FirstOrDefault(c => c.IsDeleted == false && c.Id == id);
-            if (teacher == null) return NotFound();
-            return View(teacher);
+            if (events == null) return NotFound();
+            return View(events);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Update(int? id, Teacher teacher)
         {
-            Teacher viewTeacher = _db.Teachers.Include(c => c.TeacherDetail).FirstOrDefault(c => c.Id == id && c.IsDeleted == false);
-            bool isExist = _db.Courses.Where(c => c.HasDeleted == false)
-                   .Any(c => c.Name.ToLower() == teacher.Name.ToLower());
+            LatestPostDetail viewEvents = _db.LatestPostDetails.Include(c => c.BestDetailesWorkshop).FirstOrDefault(c => c.Id == id && c.IsDeleted == false);
+            bool isExist = _db.LatestPostDetails.Where(c => c.IsDeleted == false)
+                   .Any(c => c.Title.ToLower() == viewEvents.Title.ToLower());
             if (isExist)
             {
-                ModelState.AddModelError("Name", "Bu kurs artiq movcuddur");
-                return View(viewTeacher);
+                ModelState.AddModelError("Name", "Bu event artiq movcuddur");
+                return View(viewEvents);
             }
             if (teacher.Photo == null)
             {
                 ModelState.AddModelError("", "Ayeee wekili elave ele!!!");
-                return View(viewTeacher);
+                return View(viewEvents);
             }
             if (!teacher.Photo.IsImage())
             {
-                ModelState.AddModelError("", "Kurs yaratmaq ucun wekil tipi yarat!!!");
-                return View(viewTeacher);
+                ModelState.AddModelError("", "Event yaratmaq ucun wekil tipi yarat!!!");
+                return View(viewEvents);
             }
             if (!teacher.Photo.MaxSize(200))
             {
                 ModelState.AddModelError("", "Wekilin olcusu 200kb-dan az olmalidi!!!");
-                return View(viewTeacher);
+                return View(viewEvents);
             }
 
             string folder = Path.Combine("assets", "img", "teacher");
             string fileName = await teacher.Photo.SaveImgAsync(_env.WebRootPath, folder);
-            teacher.Image = fileName;
-            teacher.IsDeleted = false;
-            viewTeacher.Name = teacher.Name;
+            viewEvents.Image = fileName;
+            viewEvents.IsDeleted = false;
+            viewEvents.Title = viewEvents.Title;
 
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
@@ -154,20 +154,6 @@ namespace EDUHOME.Areas.Admin.Controllers
 
         #endregion
 
-        #region Detail
-
-
-        public IActionResult Detail(int? id)
-        {
-            if (id == null) return NotFound();
-            Teacher teacher = _db.Teachers
-                .FirstOrDefault(c => c.IsDeleted == false && c.Id == id);
-            if (teacher == null) return NotFound();
-            return View(teacher);
-
-        }
-
-        #endregion
 
 
     }
--- EDUHOME/Models/LatestPostDetail.cs
+++ EDUHOME/Models/LatestPostDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,6 +13,8 @@ namespace EDUHOME.Models
         public int Id { get; set; }
         [Required]
         public string Image { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
         public int Day { get; set; }
         public string Month { get; set; }
         public string Title { get; set; }
