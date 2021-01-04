119829f9f0aacd0a26beab66b78af65a2abf9e33
VasifXammedov <vasifvkh@code.edu.az>
2021-01-04 20:00:16 +0400

    WelcomeToEdu modelinin crudunu yaratdiq

---
+++ EDUHOME/Areas/Admin/Controllers/WelcomeToEdusController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EDUHOME.DAL;
+using EDUHOME.Models;
+
+namespace EDUHOME.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WelcomeToEdusController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WelcomeToEdusController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/WelcomeToEdus
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.WelcomeToEdus.ToListAsync());
+        }
+
+        // GET: Admin/WelcomeToEdus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEdu = await _context.WelcomeToEdus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (welcomeToEdu == null)
+            {
+                return NotFound();
+            }
+
+            return View(welcomeToEdu);
+        }
+
+        // GET: Admin/WelcomeToEdus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/WelcomeToEdus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Image,Title,Description,IsDeleted,TimeDeleted")] WelcomeToEdu welcomeToEdu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(welcomeToEdu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(welcomeToEdu);
+        }
+
+        // GET: Admin/WelcomeToEdus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEdu = await _context.WelcomeToEdus.FindAsync(id);
+            if (welcomeToEdu == null)
+            {
+                return NotFound();
+            }
+            return View(welcomeToEdu);
+        }
+
+        // POST: Admin/WelcomeToEdus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Image,Title,Description,IsDeleted,TimeDeleted")] WelcomeToEdu welcomeToEdu)
+        {
+            if (id != welcomeToEdu.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(welcomeToEdu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WelcomeToEduExists(welcomeToEdu.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(welcomeToEdu);
+        }
+
+        // GET: Admin/WelcomeToEdus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEdu = await _context.WelcomeToEdus
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (welcomeToEdu == null)
+            {
+                return NotFound();
+            }
+
+            return View(welcomeToEdu);
+        }
+
+        // POST: Admin/WelcomeToEdus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var welcomeToEdu = await _context.WelcomeToEdus.FindAsync(id);
+            _context.WelcomeToEdus.Remove(welcomeToEdu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WelcomeToEduExists(int id)
+        {
+            return _context.WelcomeToEdus.Any(e => e.Id == id);
+        }
+    }
+}
