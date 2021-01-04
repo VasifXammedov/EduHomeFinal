c747f1480e0c0dc99733591daf0d18860d159a6f
VasifXammedov <vasifvkh@code.edu.az>
2021-01-04 19:31:12 +0400

    Profession,Notice ve Slider modellerinin crudu

---
+++ EDUHOME/Areas/Admin/Controllers/ProfessionsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EDUHOME.DAL;
+using EDUHOME.Models;
+
+namespace EDUHOME.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProfessionsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProfessionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Professions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Professions.ToListAsync());
+        }
+
+        // GET: Admin/Professions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profession = await _context.Professions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (profession == null)
+            {
+                return NotFound();
+            }
+
+            return View(profession);
+        }
+
+        // GET: Admin/Professions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Professions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,IsDeleted,TimeDeleted")] Profession profession)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(profession);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profession);
+        }
+
+        // GET: Admin/Professions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profession = await _context.Professions.FindAsync(id);
+            if (profession == null)
+            {
+                return NotFound();
+            }
+            return View(profession);
+        }
+
+        // POST: Admin/Professions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsDeleted,TimeDeleted")] Profession profession)
+        {
+            if (id != profession.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(profession);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfessionExists(profession.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profession);
+        }
+
+        // GET: Admin/Professions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profession = await _context.Professions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (profession == null)
+            {
+                return NotFound();
+            }
+
+            return View(profession);
+        }
+
+        // POST: Admin/Professions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var profession = await _context.Professions.FindAsync(id);
+            _context.Professions.Remove(profession);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfessionExists(int id)
+        {
+            return _context.Professions.Any(e => e.Id == id);
+        }
+    }
+}
